f38d171ec86e8f58aa4ad98aab8f8fcbf50ee7a6
Joeway <joewaybanh@hotmail.com>
2020-10-03 14:58:58 +0200

    cowsay exercise

---
+++ index.js
@@ -0,0 +1,8 @@
+const myImportedInfo = require("./information.js");
+const cowSay = require("cowsay")
+
+console.log(cowsay.say({
+	text : "Hello I'm Joeway",
+	e : "oO",
+	T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,10 @@
+let myInfo = {
+    myName: "Joeway",
+    myCampus: "Amsterdam"
+};
+
+function informing(){
+    console.log(`My name is ${myInfo.myName} and I'm from the ${myInfo.myCampus} campus!`)
+}
+module.exports = informing()
+
